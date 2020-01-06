8c4179e0883b104567b3a3567e8923123a004aed
Matthew Bryant <matt23488@gmail.com>
2020-01-06 15:26:19 -0500

    Some validation feedback

---
+++ scripts/Components/TimePicker.js
@@ -0,0 +1,22 @@
+﻿(function () {
+
+    const timePickerObj = {};
+
+    timePickerObj.backspace = function (element) {
+        element.value = element.value.substring(0, element.value.length - 1);
+        return element.value;
+    };
+
+    timePickerObj.addCharacter = function (element, character) {
+        element.value += character;
+        return element.value;
+    };
+
+    timePickerObj.toUpper = function (element) {
+        element.value = element.value.toUpperCase();
+        return element.value;
+    };
+
+    window.timePickerHelper = timePickerObj;
+
+})();
