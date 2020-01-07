4b6c513f840500549e52a0c17bb9d98aa06edf4e
Matthew Bryant <matt23488@gmail.com>
2020-01-06 21:47:11 -0500

    Completed the Employee grouping view and also other styling. Pretty happy with the results. Might look into alternatives for a checkbox.

--- src/EmployeeScheduler/EmployeeScheduler.Lib/DTO/ScheduleWeek.cs
+++ src/EmployeeScheduler/EmployeeScheduler.Lib/DTO/ScheduleWeek.cs
@@ -9,15 +9,6 @@ namespace EmployeeScheduler.Lib.DTO
     public class ScheduleWeek
     {
         public long ID { get; set; }
-        //public long ID => Date.Ticks;
-        //public DateTime Date { get; set; }
-        //public ScheduleDay Sunday { get; set; } = new ScheduleDay();
-        //public ScheduleDay Monday { get; set; } = new ScheduleDay();
-        //public ScheduleDay Tuesday { get; set; } = new ScheduleDay();
-        //public ScheduleDay Wednesday { get; set; } = new ScheduleDay();
-        //public ScheduleDay Thursday { get; set; } = new ScheduleDay();
-        //public ScheduleDay Friday { get; set; } = new ScheduleDay();
-        //public ScheduleDay Saturday { get; set; } = new ScheduleDay();
         public ScheduleDay Sunday { get; set; } = new ScheduleDay();
         public ScheduleDay Monday { get; set; } = new ScheduleDay();
         public ScheduleDay Tuesday { get; set; } = new ScheduleDay();
