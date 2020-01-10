d54da7e1fde43f3004092bf801989d4954e96746
Matthew Bryant <matt23488@gmail.com>
2020-01-09 22:35:59 -0500

    Trying to get an api set up to run in Azure. Still learning Blazor/JavaScript interop. Making a simple AJAX request is not as simple as it sounds.

---
+++ src/EmployeeScheduler/EmployeeScheduler.Api/Startup.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace EmployeeScheduler.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        private const string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins, builder =>
+                {
+                    builder.WithOrigins("https://localhost:44314",
+                                        "https://matt12388.github.io");
+
+                    builder.WithHeaders("Content-Type");
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseCors(MyAllowSpecificOrigins);
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ src/EmployeeScheduler/EmployeeScheduler.Web/wwwroot/scripts/testing.js
@@ -0,0 +1,15 @@
+﻿(function () {
+
+    //fetch('', {
+    //    method: 'POST',
+    //    cache: 'no-cache',
+    //    headers: {
+    //        'Content-Type': 'application/json'
+    //    }
+    //})
+
+    window.wrappedFetch = function (url, obj) {
+        return fetch(url, JSON.parse(obj));
+    };
+
+})();
