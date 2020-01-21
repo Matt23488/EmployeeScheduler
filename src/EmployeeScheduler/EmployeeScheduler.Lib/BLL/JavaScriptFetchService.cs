3e8d037a445831c6caad9d1b514f529471ff9621
Matthew Bryant <matt23488@gmail.com>
2020-01-21 14:32:01 -0500

    Still need to completely abstract away the token stuff. But this is a good intermediate step.

--- src/EmployeeScheduler/EmployeeScheduler.Lib/BLL/JavaScriptFetchService.cs
+++ src/EmployeeScheduler/EmployeeScheduler.Lib/BLL/JavaScriptFetchService.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace EmployeeScheduler.Lib.BLL
 {
@@ -20,17 +21,35 @@ namespace EmployeeScheduler.Lib.BLL
 
         public async Task<T> GetAsync<T>(string url) => await GetAsync<T>(url, null, null);
         public async Task<T> GetAsync<T>(string url, object body) => await GetAsync<T>(url, body, null);
-        public async Task<T> GetAsync<T>(string url, object body, Dictionary<string, string> additionalHeaders) => await DoRequestAsync<T>(url, body, additionalHeaders, "GET");
+        public async Task<T> GetAsync<T>(string url, object body, Dictionary<string, string> additionalHeaders)
+        {
+            var containsQueryStringAlready = url.Split("?").Length > 1;
+            var properties = body?.GetType()?.GetProperties() ?? new System.Reflection.PropertyInfo[0];
+            var newUrlBuilder = new StringBuilder(url);
+            for (int i = 0; i < properties.Length; i++)
+            {
+                if (i == 0 && !containsQueryStringAlready) newUrlBuilder.Append("?");
+                else newUrlBuilder.Append("&");
+
+                newUrlBuilder.Append($"{HttpUtility.UrlEncode(properties[i].Name)}={Convert.ToString(properties[i].GetValue(body))}");
+            }
+
+            var data = new FetchData
+            {
+                method = "GET"
+            };
+            additionalHeaders?.ToList()?.ForEach(kvp => data.headers[kvp.Key] = kvp.Value);
+
+            return await _js.InvokeAsync<T>("blazorFetchService.fetch", newUrlBuilder.ToString(), data);
+        }
 
         public async Task<T> PostAsync<T>(string url) => await PostAsync<T>(url, null, null);
         public async Task<T> PostAsync<T>(string url, object body) => await PostAsync<T>(url, body, null);
-        public async Task<T> PostAsync<T>(string url, object body, Dictionary<string, string> additionalHeaders) => await DoRequestAsync<T>(url, body, additionalHeaders, "POST");
-
-        public async Task<T> DoRequestAsync<T>(string url, object body, Dictionary<string, string> additionalHeaders, string method)
+        public async Task<T> PostAsync<T>(string url, object body, Dictionary<string, string> additionalHeaders)
         {
             var data = new FetchData
             {
-                method = method,
+                method = "POST",
                 body = JsonConvert.SerializeObject(body)
             };
             additionalHeaders?.ToList()?.ForEach(kvp => data.headers[kvp.Key] = kvp.Value);
--- src/EmployeeScheduler/EmployeeScheduler.Web/wwwroot/scripts/Fetch/Fetch.js
+++ src/EmployeeScheduler/EmployeeScheduler.Web/wwwroot/scripts/Fetch/Fetch.js
@@ -2,33 +2,12 @@
 
     const fetchObj = {};
 
-    fetchObj.fetch = function (url, obj) {
-        //return fetch(url, JSON.parse(obj)).then(r => {
-        //    if (r.status !== 200) {
-        //        console.log(r.status, r.statusText);
-        //        throw new Error(r.statusText);
-        //    }
-        //    return r.json();
-        //});
-
-        console.log(url, obj);
-        return new Promise(resolve => {
-            resolve({
-                status: 401,
-                json: JSON.stringify({
-                    type: "type",
-                    title: "title",
-                    status: 401,
-                    traceId: "???"
-                })
-            });
-        });
-        return fetch(url, JSON.parse(obj)).then(async r => {
-            return {
-                status: r.status,
-                json: await r.text()
-            };
-        });
+    fetchObj.fetch = async function (url, obj) {
+        const r = await fetch(url, obj);
+        return {
+            status: r.status,
+            json: await r.text()
+        };
     };
 
     window.blazorFetchService = fetchObj;
